23ad007aaa86146997d53664306024c09f0c4b6e
Imad Atyat-Allah <imadatyatalah@gmail.com>
2023-04-17 23:12:30 +0000

    fix: console error

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -9,23 +9,34 @@ import SunIcon from "@/icons/SunIcon";
 
 import Logo from "public/logo.png";
 
-const ThemeToggle = ({ theme }: { theme?: string }) => {
+const ThemeToggle = () => {
   const [mounted, setMounted] = useState(false);
 
-  useEffect(() => setMounted(true), []);
-
-  if (!mounted) return <div className="h-5 w-5"></div>;
-
-  return <>{theme === "light" ? <MoonIcon /> : <SunIcon />}</>;
-};
-
-const Header = () => {
   const { theme, resolvedTheme, setTheme } = useTheme();
 
   const handleMoodToggle = () => {
     setTheme(resolvedTheme === "light" ? "dark" : "light");
   };
 
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return <div className="h-5 w-5"></div>;
+
+  return (
+    <div>
+      <button
+        onClick={handleMoodToggle}
+        type="button"
+        className="rounded-lg p-2 hover:bg-gray-200 dark:hover:bg-gray-700"
+        aria-label={resolvedTheme === "light" ? "Dark Mode" : "Light Mode"}
+      >
+        {theme === "light" ? <MoonIcon /> : <SunIcon />}
+      </button>
+    </div>
+  );
+};
+
+const Header = () => {
   return (
     <header className="py-2">
       <div className="flex items-center justify-between">
@@ -54,18 +65,7 @@ const Header = () => {
             </ul>
           </nav>
 
-          <div>
-            <button
-              onClick={handleMoodToggle}
-              type="button"
-              className="rounded-lg p-2 hover:bg-gray-200 dark:hover:bg-gray-700"
-              aria-label={
-                resolvedTheme === "light" ? "Dark Mode" : "Light Mode"
-              }
-            >
-              <ThemeToggle theme={theme} />
-            </button>
-          </div>
+          <ThemeToggle />
         </div>
       </div>
     </header>
