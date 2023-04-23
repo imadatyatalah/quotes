a4c12e6ee9bbaccccf412fec017ce2b5ee7f0dea
Imad Atyat-Allah <imadatyatalah@gmail.com>
2023-04-23 18:26:48 +0100

    cleanup

--- src/modules/random/ShareQuoteModal.tsx
+++ src/modules/random/ShareQuoteModal.tsx
@@ -12,46 +12,43 @@ import {
 
 import { TwitterLogoIcon } from "@radix-ui/react-icons";
 
-const ShareQuoteModal = ({
-  quote,
-  author,
-}: {
+type Props = {
   quote: string;
   author: string;
-}) => {
+};
+
+const ShareQuoteModal = ({ quote, author }: Props) => {
   const twitterShareURL = twitterShareQuoteURL(quote, author);
 
   return (
-    <>
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="primary" type="button">
-            <ShareIcon className="mr-2" /> Share
-          </Button>
-        </DialogTrigger>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="primary" type="button">
+          <ShareIcon className="mr-2" /> Share
+        </Button>
+      </DialogTrigger>
 
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Share quote</DialogTitle>
-            <DialogDescription>
-              Choose a sharing option below to spread the inspiration!
-            </DialogDescription>
-          </DialogHeader>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Share quote</DialogTitle>
+          <DialogDescription>
+            Choose a sharing option below to spread the inspiration!
+          </DialogDescription>
+        </DialogHeader>
 
-          <div className="flex items-center space-x-2">
-            {/* Twitter */}
-            <a
-              href={twitterShareURL}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="rounded-full bg-[#1DA1F2] p-5"
-            >
-              <TwitterLogoIcon className="h-6 w-6 text-white" />
-            </a>
-          </div>
-        </DialogContent>
-      </Dialog>
-    </>
+        <div className="flex items-center space-x-2">
+          {/* Twitter */}
+          <a
+            href={twitterShareURL}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="rounded-full bg-[#1DA1F2] p-5"
+          >
+            <TwitterLogoIcon className="h-6 w-6 text-white" />
+          </a>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 };
 
