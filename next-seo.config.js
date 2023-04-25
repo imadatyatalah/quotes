27f2676e4874f16e105db21eab711b7e2b5e622b
Imad Atyat-Allah <imadatyatalah@gmail.com>
2023-04-25 20:50:45 +0100

    feat: add og image

--- next-seo.config.js
+++ next-seo.config.js
@@ -7,11 +7,16 @@ const NextSeo = {
     type: "website",
     url: "https://quotes.imadatyat.me",
     site_name: "Quotes",
+    images: [
+      {
+        url: "https://quotes.imadatyat.me/og.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
   },
 
   twitter: {
-    handle: "",
-    site: "",
     cardType: "summary_large_image",
   },
 };
